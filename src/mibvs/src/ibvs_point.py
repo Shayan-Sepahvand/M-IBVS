6a67d77f06bdaec2e6d98ce2e8c843a719965d58
ShayanSP <96470390+Shayan-Sepahvand@users.noreply.github.com>
2026-04-27 20:49:40 -0400

    Refactor IBVS controller and callback structure
    Refactored the IBVS controller by introducing a Controller class, updating callback function, and modifying Jacobian and control law methods.

--- src/mibvs/src/ibvs_point.py
+++ src/mibvs/src/ibvs_point.py
@@ -1,364 +1,102 @@
-#!/usr/bin/env python3      
+#!/usr/bin/env python3  
 import rospy
-import cv2 as cv
-import matplotlib.pyplot as plt
 import numpy as np
-import time
-
-from sensor_msgs.msg import Image
-from geometry_msgs.msg import Twist, PoseStamped, Pose
-from cv_bridge import CvBridge
-import message_filters
-from sensor_msgs.msg import Image, CameraInfo
-from ar_track_alvar_msgs.msg import AlvarMarkers
-from visualization_msgs.msg import Marker
-from geometry_msgs.msg  import Vector3
-from std_msgs.msg import Float32
-from nav_msgs.msg import Odometry
-import tf.transformations as tf
-
-# init_time = time.time() 
-def callBack(image_data, marker_data, info_data):
-    #########################################
-    global loop_index, w, THETA, rList, arlist, init_time, m, v
-    # print("Time:", time.time() - init_time)
-    loop_index = loop_index + 1
-    t = rospy.get_time()
-    # print(t)
-    #########################################
-    # start_time = time.time()
-    ######################################### Read the imagefrom ROS topic and convert it to opencv using a bridge  
-    bridge = CvBridge()
-    cv_image = bridge.imgmsg_to_cv2(image_data, desired_encoding = 'passthrough')
-    ######################################### Initializing a binary image using the centers
-    imsize = cv_image.shape
-    # print(imsize)
-    # binary_image_to_use = np.zeros((imsize[0], imsize[1]), np.float64)
-    ######################################### Extract the cam_intrinsics from the ros message
-    #cam_intrinsics (a 1D tuple)
-    intrinsics = info_data.K 
-    fx = intrinsics[0]
-    fy = intrinsics[4]
-    px = intrinsics[2]
-    py = intrinsics[5]
-    ########################################## Extract the marker ID from /ar_pose_marker topic
-    position = {}
-    pixel_coordinates_for_cv = {}
-    ########################################## Circles Specifications
-    # radius = 5 # Radius of the circle
-    # color = {1: (0, 255, 0), 2: (255, 0, 0), 3: (0, 0, 255), 4: (0, 127, 127)} # Color in BGR format
-    # thickness = 2  # Thickness of the circle outline
-    ########################################## Extract the marker id and center postion
-    Norm_Cords_Mat = np.zeros((4,2))
-    # binary_image = np.zeros((imsize[0], imsize[1], 3), np.uint8)
-    Lp = np.zeros((8,6))
-    e_feat = np.zeros((8,1))
-    # desired normalized u and v arrange as 8 by 1 matrix (u1 v1 u2 v2 u3 v3 u4 v4)
-    des_feat = np.array([[0.11405071],
-                         [0.11441344],
-                         [-0.12223703],
-                         [0.11388478],
-                         [0.11406292],
-                         [-0.12031294],
-                         [-0.12215015], 
-                         [-0.12058573]])
-    
-#     C[[0.11405071]
-#  [0.11441344]]
-# [[-0.12223703]
-#  [ 0.11388478]]
-# [[ 0.11406292]
-#  [-0.12031294]]
-# [[-0.12215015]
-#  [-0.12058573]]
-
-    id = 1000
-    for i in range(0,4):
-        try:
-            marker_info = marker_data.markers[i].pose
-            id = marker_data.markers[i].id
-        except:
-            print(f'The features with ID = {id} is lost.')
-            
-        # The poistition of the markers in the camera frame
-        xi = marker_info.pose.position.x
-        yi = marker_info.pose.position.y
-        zi = marker_info.pose.position.z
-    
-        
-        xnorm = (xi/zi) 
-        ynorm = (yi/zi) 
-        norm_cor = np.array([[xnorm],[ynorm]])
-        # print(norm_cor)
-        # Norm_Cords_Mat[id-1,:] = np.squeeze(norm_cor, axis=1)
-        # print(xnorm, ynorm)
-        
-        if i == 0:
-            Lp[0:2,:] = np.array([[-1/zi,  0, xnorm/zi, xnorm*ynorm, -(1+xnorm**2), ynorm],
-                                  [0, -1/zi, ynorm/zi, (1+ynorm**2), -xnorm*ynorm, -xnorm]])
-            e_feat[0:2,:] = des_feat[0:2,:] - norm_cor
-            
-        if i == 1:
-            Lp[2:4,:] = np.array([[-1/zi,  0, xnorm/zi, xnorm*ynorm, -(1+xnorm**2), ynorm],
-                                  [0, -1/zi, ynorm/zi, (1+ynorm**2), -xnorm*ynorm, -xnorm]])
-            e_feat[2:4,:] =   des_feat[2:4,:] - norm_cor
-        
-        if i == 2:
-            Lp[4:6,:] = np.array([[-1/zi,  0, xnorm/zi, xnorm*ynorm, -(1+xnorm**2), ynorm],
-                                  [0, -1/zi, ynorm/zi, (1+ynorm**2), -xnorm*ynorm, -xnorm]])
-            e_feat[4:6,:] = des_feat[4:6,:] - norm_cor
-            
-        if i == 3:
-            Lp[6:8,:] = np.array([[-1/zi,  0, xnorm/zi, xnorm*ynorm, -(1+xnorm**2), ynorm],
-                                  [0, -1/zi, ynorm/zi, (1+ynorm**2), -xnorm*ynorm, -xnorm]])
-            e_feat[6:8,:] =  des_feat[6:8,:] - norm_cor
-      
-    print(f'{loop_index} Running ...')    
-
-    ###################################################
-    global feature_error_publisher
-    ferror = Vector3()
-    ferror.x = e_feat[0,0]
-    ferror.y = e_feat[1,0]
-    ferror.z = 0
-    
-    # print("error vector:", ({e_feat[0,0]},{e_feat[1,0]}))
-    feature_error_publisher.publish(ferror)
-    
-    Error_List.append(e_feat)
-    t = rospy.get_time()
-    Time_List.append(t)
-    dt = Time_List[loop_index] - Time_List[loop_index-1]
-    error_integral = integrator(Error_List, dt)
-    
-    
-    # theta, flag = connecting_line(Norm_Cords_Mat[0:2,:])
-
-    # if flag == True:
-    #     theta = THETA[loop_index-10]
-        
-    # THETA.append(theta)   
-     
-    print(f'{loop_index} Running ...')    
-    
-    
-    # qa = theta 
-    # qa_des = 0
-    # error_qa = qa_des - qa
-    # ua = yaw_controller(error_qa)
-    
-    
-    
-    lam = 0.08
-    u_f = lam * np.linalg.pinv(Lp) @ e_feat
-    print(u_f)
-    # u_f = -lam * np.linalg.pinv(Lp) @ e_feat
-    # compen = Lp[:,3] * ua
-    # compen = np.expand_dims(compen, axis=1)
-    # u_f = lam * Lp.transpose()  @ (e_feat - compen)
-    # u_f = lam * Lp.transpose() @ e_feat + 0.01 * Lp.transpose() @ error_integral
-    # u_f = lam * Lp.transpose() @ e_feat + 0.1 * Lp.transpose() @ (np.tanh(10*e_feat/np.abs(e_feat)))
-    
-    # this is the twist vector expressed in the camera frame but we need to transform it to the uav frame
-    # it is also possible to transform it to the global frame.
-    # uf is expressed in the camera frame
-    # AdT --> the adjoint transformation from the cam to uav frame.
-    
-    # print("Relative Pose:",{rel_pose_data})
-    # RRR = np.array([[ 0, 0, 1],
-    #                 [-1,  0, 0],
-    #                 [ 0,  -1, 0]])
-    # use rviz visulizatoin of tfs to figure this out.
-    RRR = np.array([[ 1,  0,  0],
-                    [ 0,  0,  1],
-                    [ 0, -1,  0]])
-    
-    # camera_offset_len (this is inside the bebop_base.urdf.xacro)
-    ttt = np.array([[0],[0.1],[0]])    
-    AdT = adjoint_matrix(RRR, ttt)
-    
-
-    # AdT = np.array([[0, -1,  0,  0,  0,  0],
-    #                 [-1,  0,  0,  0,  0,  0],
-    
-    #                 [ 0,  0, -1,  0,  0,  0],
-    #                 [ 0,  0,  0,  0, -1,  0],
-    #                 [ 0,  0,  0, -1,  0,  0],
-    #                 [ 0,  0,  0,  0,  0, -1]])
-    # u_F = np.array([u_f[0,0],u_f[1,0],u_f[2,0],0,0,u_f[3,0]])  # in camera coordinates [vx vy vz 0 0 wz]
-    
-    u_f = AdT @ u_f# twist expressed in the uav(body) frame
-    
-    # yaw_feature_pub.publish(error_qa)
-    
-    
-    # print(u_f)
-    ######################################### Publish the velocity command to the UAV
-    global pub
-    twist = Twist()
-    twist.linear.x  = u_f[0]
-    twist.linear.y  = u_f[1]
-    twist.linear.z  = u_f[2]
-    twist.angular.z = u_f[5]
-
-    # twist.angular.z = ua
-    pub.publish(twist)
-    print(twist)
-
-    ########################################### end of the main callback
-
-
-
-
-
-
-
-
-
-
-
-########################################### Perception Blocks 
-########################################### End of Perception Blocks 
-########################################### End of Perception Blocks 
-########################################### End of Perception Blocks 
-
-
-########################################### Control Block
-########################################### Control Block 
-########################################### Control Block 
-
-
-
-def skew(x):
-    sk = np.array([[0, -x[2,0], x[1,0]],[x[2,0], 0, -x[0,0]], [-x[1,0], x[0,0], 0]])
-    return sk
-
-
-
-##################################################################################### End of Neural Network Blocks
-
-
 import math
-# def quat2eulers(q0:float, q1:float, q2:float, q3:float) -> tuple:
-#     """
-#     Compute yaw-pitch-roll Euler angles from a quaternion.
-    
-#     Args
-#     ----
-#         q0: Scalar component of quaternion.
-#         q1, q2, q3: Vector components of quaternion.
-    
-#     Returns
-#     -------
-#         (roll, pitch, yaw) (tuple): 321 Euler angles in radians
-#     """
-#     roll = math.atan2(
-#         2 * ((q2 * q3) + (q0 * q1)),
-#         q0**2 - q1**2 - q2**2 + q3**2
-#     )  # radians
-#     pitch = math.asin(2 * ((q1 * q3) - (q0 * q2)))
-#     yaw = math.atan2(
-#         2 * ((q1 * q2) + (q0 * q3)),
-#         q0**2 + q1**2 - q2**2 - q3**2
-#     )
-#     return (roll, pitch, yaw)
+from geometry_msgs.msg import Twist
+from ar_track_alvar_msgs.msg import AlvarMarkers
 
-def euler_from_quaternion(x, y, z, w):
-        """
-        Convert a quaternion into euler angles (roll, pitch, yaw)
-        roll is rotation around x in radians (counterclockwise)
-        pitch is rotation around y in radians (counterclockwise)
-        yaw is rotation around z in radians (counterclockwise)
-        """
-        t0 = +2.0 * (w * x + y * z)
-        t1 = +1.0 - 2.0 * (x * x + y * y)
-        roll_x = math.atan2(t0, t1)
-     
-        t2 = +2.0 * (w * y - z * x)
-        t2 = +1.0 if t2 > +1.0 else t2
-        t2 = -1.0 if t2 < -1.0 else t2
-        pitch_y = math.asin(t2)
-     
-        t3 = +2.0 * (w * z + x * y)
-        t4 = +1.0 - 2.0 * (y * y + z * z)
-        yaw_z = math.atan2(t3, t4)
-     
-        return (roll_x, pitch_y, yaw_z) # in radians
-########################################### Control Block
-########################################### Control Block 
-########################################### Control Block 
-def adjoint_matrix(R,t):
+class Controller():
+    def __init__(self, KP):
+        self.kp = KP
+        # Camera to UAV Rotation
+        self.Rs = np.array([[ 1,  0,  0],
+                            [ 0,  0,  1],
+                            [ 0, -1,  0]])
+        self.camera_offset = 0.1
+        # Camera offset translation (converted to 3x1 for skew math)
+        self.t = np.array([[0.0], [self.camera_offset], [0.0]])    
+
+    def skew(self, x):
+        # x comes in as (3,1), we need to extract scalars
+        return np.array([[0, -x[2,0], x[1,0]],
+                         [x[2,0], 0, -x[0,0]], 
+                         [-x[1,0], x[0,0], 0]])
+
+    def adjoint_matrix(self):
+        tcross = self.skew(self.t)
+        AdT_r1 = np.hstack((self.Rs, np.zeros((3, 3))))
+        AdT_r2 = np.hstack((tcross @ self.Rs, self.Rs))
+        AdT = np.vstack((AdT_r1, AdT_r2))
+        return AdT
+
+    def im_jacob(self, marker_data, des_feat):
+        Lp = np.zeros((8,6))
+        e_feat = np.zeros((8,1)) 
+        
+        # Guard: Check if we actually have enough markers
+        num_markers = len(marker_data.markers)
+        if num_markers < 4:
+            rospy.logwarn_once("Waiting for 4 markers...")
+            return None, None
+
+        for i in range(0, 4):
+            marker_info = marker_data.markers[i].pose.pose
+            xi = marker_info.position.x
+            yi = marker_info.position.y
+            zi = marker_info.position.z
+        
+            xnorm = xi/zi
+            ynorm = yi/zi
+            norm_cor = np.array([[xnorm],[ynorm]])
+            
+            # Fill Jacobian and Error rows
+            row = i * 2
+            Lp[row:row+2,:] = np.array([[-1/zi,  0, xnorm/zi, xnorm*ynorm, -(1+xnorm**2), ynorm],
+                                        [0, -1/zi, ynorm/zi, (1+ynorm**2), -xnorm*ynorm, -xnorm]])
+            e_feat[row:row+2,:] = des_feat[row:row+2,:] - norm_cor
+                    
+        return Lp, e_feat
     
-    # Extract rotation matrix (3x3) and translation vector
-    tcross = skew(t)
-    # Create the first row block
-    AdT_r1 = np.hstack((R, np.zeros((3, 3))))
-    # Create the second row block
-    AdT_r2 = np.hstack((tcross @ R, R))
-    # Stack the blocks vertically to form a 6x6 matrix
-    AdT = np.vstack((AdT_r1, AdT_r2))
+    def ibvs_crtl_law(self, Lp, es):   
+        # Simple proportional control with pseudo-inverse
+        return self.kp * np.linalg.pinv(Lp) @ es
+
+# Main Callback -------------------------------------------------------------------------
+
+def callBack(marker_data, args):
+    ctrl, pub = args
     
-    return AdT
-
-def integrator(xhist, dt):
-    xhist = np.array(xhist)
+    # Desired features (u1, v1... u4, v4)
+    des_feat = np.array([[0.114], [0.114], [-0.122], [0.113], 
+                         [0.114], [-0.120], [-0.122], [-0.120]])
     
-    xint = np.sum(xhist, axis = 0) * dt
-    return xint
+    Lp, es = ctrl.im_jacob(marker_data, des_feat)
+    
+    if Lp is not None:
+        uf_cam = ctrl.ibvs_crtl_law(Lp, es)  
+        AdT = ctrl.adjoint_matrix()
+        uf_body = AdT @ uf_cam # Transform twist to UAV body frame
+        
+        twist = Twist()
+        twist.linear.x  = uf_body[0]
+        twist.linear.y  = uf_body[1]
+        twist.linear.z  = uf_body[2]
+        twist.angular.z = uf_body[5]
+        pub.publish(twist)
 
-def connecting_line(normalized_cord):
-    flag = False
-    try:
-        x1, y1 = normalized_cord[0,:]
-        x2, y2 = normalized_cord[1,:]
-        theta = np.arctan2((y2 - y1), (x2 - x1))
-        theta = theta % (2*np.pi)
-    except:
-        theta = 0
-        flag = True
-    return theta, flag  
+# Main ----------------------------------------------------------------------------------
 
-def yaw_controller(e):
-    eta = 0.05
-    # kd = 0#control gain
-    # s =  e + kd * edot 
-    # u = eta * np.sqrt(np.abs(s)) * np.sign(s)
-    u = eta*e
-    return u
-
-###################################################################################### ROS Network
 if __name__ == '__main__':
-    
-    global loop_index, Error_List, Contrl_Input_List, Time_List, Yaw_Error_List, w, THETA, rList, yaw_control_input_list, position_mat, orientaion_mat
-    global pose_time, arlist
-    global m, v
-    ##############################################
-    loop_index = 0
-    Error_List = [np.zeros((8,1))]
-    Time_List = [0]
-    THETA = []
-
-
-    rospy.init_node('sepahvand_node', anonymous=True)
-    point_publisher = rospy.Publisher("image_point", Marker, queue_size=1)
-    
-    image_sub = message_filters.Subscriber('bebop/image_raw', Image)
-    marker_sub= message_filters.Subscriber('ar_pose_marker', AlvarMarkers)
-    camera_info_sub = message_filters.Subscriber('/bebop/camera_info', CameraInfo) 
-    
-    rel_pose_sub = message_filters.Subscriber('/cam_pose_in_uav_frame', PoseStamped) 
-    
-    
-    ts = message_filters.TimeSynchronizer([image_sub, marker_sub, camera_info_sub], 1, 1)
-    ts.registerCallback(callBack)
-    
-    # uav_pose_info = rospy.Subscriber('/bebop/odom', Odometry, callback = pose_callback) 
-    
-    
-    feature_error_publisher = rospy.Publisher("feature_error", Vector3, queue_size=1)
-    # yaw_feature_pub = rospy.Publisher("qa", Float32, queue_size=1)
-    # norm_one_ferror = rospy.Publisher("norm", Float32, queue_size=1)
-    pub = rospy.Publisher("bebop/cmd_vel", Twist, queue_size = 1)
-    
-    rospy.spin()
-######################################################################################End of ROS Network
+    try:
+        rospy.init_node('bebop_ibvs_controller')
+        
+        ctrl = Controller(KP=0.08)
+        pub = rospy.Publisher("bebop/cmd_vel", Twist, queue_size=1)
+        
+        # Passing ctrl and pub into the callback safely
+        rospy.Subscriber('ar_pose_marker', AlvarMarkers, callBack, callback_args=(ctrl, pub))
+        
+        rospy.loginfo("IBVS Node Started")
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
